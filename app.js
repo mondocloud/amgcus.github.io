5451f21111b30c1f68fe42886348ba9e03955e20
mondocloud <164967526+mondocloud@users.noreply.github.com>
2025-08-14 05:34:03 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+// Firebase config placeholder
+const firebaseConfig = { apiKey: "YOUR_API_KEY", authDomain: "YOUR_PROJECT.firebaseapp.com", projectId: "YOUR_PROJECT", storageBucket: "YOUR_PROJECT.appspot.com", appId: "YOUR_APP_ID" };
+// More code here from the previous message...
